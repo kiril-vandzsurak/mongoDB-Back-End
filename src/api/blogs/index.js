94f0004ee91c43b619f18ca1f5a7b5fcd1480193
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2023-01-17 14:57:57 +0100

    Creating endpoint with POST on MongoDB

---
+++ src/api/blogs/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import BlogModel from "./models.js";
+
+const blogsRouter = express.Router();
+
+blogsRouter.post("/", async (req, res, next) => {
+  try {
+    const newBlog = new BlogModel(req.body);
+    const { _id } = await newBlog.save();
+    res.status(201).send({ _id });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default blogsRouter;
--- src/server.js
+++ src/server.js
@@ -2,9 +2,15 @@ import express from "express";
 import listEndpoints from "express-list-endpoints";
 import cors from "cors";
 import mongoose from "mongoose";
+import blogsRouter from "./api/blogs/index.js";
 
 const server = express();
 const port = process.env.PORT || 3001;
+
+server.use(cors());
+server.use(express.json());
+
+server.use("/blogs", blogsRouter);
 
 mongoose.connect(process.env.MONGO_KEY);
 
