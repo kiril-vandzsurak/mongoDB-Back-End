aa52a7877d54557fd1e522320253794da3da4f4a
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2023-02-07 14:43:56 +0100

    Creating hashed password creator

--- src/api/authors/models.js
+++ src/api/authors/models.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const { Schema, model } = mongoose;
 
@@ -6,11 +7,24 @@ const authorSchema = new Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["User", "Admin"], default: "User" },
     blogs: [{ type: Schema.Types.ObjectId, ref: "Blog" }],
   },
   {
     timestamps: true,
   }
 );
+
+authorSchema.pre("save", async function (next) {
+  const currentAuthor = this;
+  if (currentAuthor.isModified("password")) {
+    const plainPW = currentAuthor.password;
+    const hash = await bcrypt.hash(plainPW, 11);
+    currentAuthor.password = hash;
+  }
+  next();
+});
 
 export default model("Author", authorSchema);
